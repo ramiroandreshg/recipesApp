de03fd690e416545d9fac329a5743070a9b8d1d0
Ramiro <ramiroandreshg@gmail.com>
2019-07-08 13:01:20 -0300

    first basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
