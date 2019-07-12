31882ed193ba1ba048db855dcc872facab8c4599
Ramiro <ramiroandreshg@gmail.com>
2019-07-12 11:49:37 -0300

    form edit progress

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,16 +12,57 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  get controls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
+
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params.id;
         this.editMode = params.id ? true : false;
+        this.initForm();
       }
     );
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    const  recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe.ingredients) {
+        for(const ing of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ing.name),
+            'amount': new FormControl(ing.amount)
+          }));
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
+    });
+  }
+
+  onSubmit() {
+    console.log('FORM', this.recipeForm);
   }
 
 }
